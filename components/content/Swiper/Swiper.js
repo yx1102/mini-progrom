ea136d1637ffddfb9cb08a4288cdcdf4e7a1490e
yx <huangyx07@126.com>
2020-05-03 00:25:27 +0800

    Initial Commit

---
+++ components/content/Swiper/Swiper.js
@@ -0,0 +1,28 @@
+// components/content/Swiper/Swiper.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 2000
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
